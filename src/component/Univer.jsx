97ef116261e2e141578b99c33bb1f739a5155540
GS-M <litvan2001@mail.ru>
2020-08-15 14:03:39 +0300

    aad Component Univer
    Вынес отрисовку в отдельную компоненту

--- src/component/Univer.jsx
+++ src/component/Univer.jsx
@@ -1,6 +1,27 @@
-//  import React from 'react';
+import React from 'react';
 
 
-// let Univer = () =>{
+let Univer = (props) => {
+    return (<div>
+        {
+            props.university.map(uni =>
 
-// }
+                <div className="university" key={uni.id}>
+                    <ul>
+                        <li><a id={uni.id} href="newPage.html" /*onClick="newP(id)"*/>{uni.name}</a><ul>
+                            <li>Бюджeтных мест: {uni.budget_places}</li>
+                            <li>Платных мест: {uni.paid_places}</li>
+                            <li>Мест для иностранцев: {uni.foreign_places}</li>
+                            <li>Минимальные баллы на бюджет: {uni.min_budget_score}</li>
+                            <li>Минимальные баллы на платку: {uni.min_paid_score}</li>
+                            <li>Стоимость обучения: {uni.price} руб.</li></ul>
+                        </li>
+                    </ul>
+                </div>
+            )
+        }
+    </div>
+    )
+}
+
+export default Univer;
